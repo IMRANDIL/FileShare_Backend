392c46f5fce5f3c2888322476209af671e93b106
IMRANDIL <aliimranadil2@gmail.com>
2021-12-29 16:44:30 +0530

    yes

--- server.js
+++ server.js
@@ -9,6 +9,8 @@ const PORT = process.env.PORT || 3000;
 
 //Template Engine..
 
+app.use(express.static('public'))
+
 app.set('views', path.join(__dirname, '/views'));
 
 app.set('view engine', 'ejs')
@@ -19,6 +21,8 @@ app.set('view engine', 'ejs')
 app.use('/api/files', require('./routes/files'))
 
 app.use('/files', require('./routes/show'));
+
+app.use('/files/download', require('./routes/download'))
 
 
 

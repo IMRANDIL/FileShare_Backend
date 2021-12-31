98a07a3c8035aaac5436751a2db39a22811ac972
IMRANDIL <aliimranadil2@gmail.com>
2021-12-31 20:15:05 +0530

    yh

--- services/emailTemplate.js
+++ services/emailTemplate.js
@@ -147,8 +147,8 @@ module.exports = ({ emailFrom, downloadLink, size, expires }) => {
                         <table border="0" cellpadding="0" cellspacing="0" style="border-collapse: separate; mso-table-lspace: 0pt; mso-table-rspace: 0pt; width: 100%;">
                             <tr>
                             <td class="content-block" style="font-family: sans-serif; vertical-align: top; padding-bottom: 10px; padding-top: 10px; font-size: 12px; color: #999999; text-align: center;">
-                                <span class="apple-link" style="color: #999999; font-size: 12px; text-align: center;">inShare inc. main street 29, Moscow</span>
-                                <br> Want to share a file? <a href="http://localhost:3000" style="text-decoration: underline; color: #999999; font-size: 12px; text-align: center;">inShare</a>.
+                                <span class="apple-link" style="color: #999999; font-size: 12px; text-align: center;">fileShare inc. Dumaria, Bistoria, India</span>
+                                <br> Want to share a file? <a href="https://imrandil.github.io/NodeExpress_Project02/" style="text-decoration: underline; color: #999999; font-size: 12px; text-align: center;">inShare</a>.
                             </td>
                             </tr>
                             <tr>
